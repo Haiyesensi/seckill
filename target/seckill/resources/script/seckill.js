dcf170ba5247e5e3dcebd64afbf99c9693f8c48c
Haiyesensi <32067085+Haiyesensi@users.noreply.github.com>
2018-08-17 13:58:01 +0800

    添加jmeter压力测试结果，打包

---
+++ target/seckill/resources/script/seckill.js
@@ -0,0 +1,100 @@
+var seckill = {
+    URL: {
+        exposer: function (seckillId) {
+            return '/seckill/' + seckillId + '/exposer';
+        },
+        execution: function (seckillId, md5) {
+            return '/seckill/' + seckillId + '/' + md5 + '/execution';
+        }
+    },
+    validataPhone: function (phone) {
+        if (phone && phone.length == 11 && !isNaN(phone)) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+    countdown: function (seckillId, nowTime, startTime, endTime) {
+        var seckillBox = $('#seckill-box');
+        if (nowTime > endTime) {
+            seckillBox.html('end.');
+        } else if (nowTime < startTime) {
+            var killTime = new Date(startTime + 1000);
+            seckillBox.countdown(killTime, function (event) {
+                var format = event.strftime('秒杀倒计时: %D天 %H时 %M分 %S秒');
+                seckillBox.html(format);
+            }).on('finish.countdown', function () {
+                seckill.handleSeckill(seckillId, seckillBox);
+            });
+        } else {
+            seckill.handleSeckill(seckillId, seckillBox);
+        }
+    },
+    handleSeckill: function (seckillId, node) {
+        node.hide().html('<button class="btn btn-primary btn-lg" id="killBtn">开始秒杀</button>');
+        $.post(seckill.URL.exposer(seckillId), {}, function (result) {
+            if (result && result['success']) {
+                var exposer = result['data'];
+                if (exposer['exposed']) {
+                    var md5 = exposer['md5'];
+                    var killUrl = seckill.URL.execution(seckillId, md5);
+                    console.log('killUrl: ' + killUrl);
+                    $('#killBtn').one('click', function () {
+                        $(this).addClass('disable');
+                        $.post(killUrl, {}, function (result) {
+                            if (result && result['success']) {
+                                var killResult = result['data'];
+                                var state = killResult['state'];
+                                var stateInfo = killResult['stateInfo'];
+                                node.html('<span class="label label-success">' + stateInfo + '</span>');
+                            }
+                        });
+                    });
+                    node.show();
+                } else {
+                    var now = exposer['now'];
+                    var start = exposer['start'];
+                    var end = exposer['end'];
+                    seckill.countdown(seckillId, now, start, end);
+                }
+            } else {
+                console.log('result: ' + result);
+            }
+        })
+    },
+    detail: {
+        init: function (params) {
+            var killPhone = $.cookie('killPhone');
+            var startTime = params['startTime'];
+            var endTime = params['endTime'];
+            var seckillId = params['seckillId'];
+            if (!seckill.validataPhone(killPhone)) {
+                var killPhoneModal = $('#killPhoneModal');
+                killPhoneModal.modal({
+                    show: true,
+                    backdrop: 'static',
+                    keyboard: false
+                });
+
+                $('#killPhoneBtn').click(function () {
+                    var inputPhone = $('#killPhoneKey').val();
+                    if (seckill.validataPhone(inputPhone)) {
+                        $.cookie('killPhone', inputPhone, {expires: 7, path: '/seckill'});
+                        window.location.reload();
+                    } else {
+                        $('#killPhoneMessage').hide().html('<label class="label label-danger">phone error!</label>').show(300);
+                    }
+                });
+            }
+            $.get('/seckill/time/now', {}, function (result) {
+                if (result && result['success']) {
+                    var nowTime = result['data'];
+                    console.log('nowtime: ' + nowTime);
+                    seckill.countdown(seckillId, nowTime, startTime, endTime);
+                } else {
+                    console.log('result: ' + result);
+                }
+            });
+        }
+    }
+}
